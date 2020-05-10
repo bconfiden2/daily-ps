312937fedf09456d17f59d5c6cab1c701acf54ca
bconfiden2 <bconfiden2@naver.com>
2020-05-11 08:17:43 +0900

    0511

--- boj/10845.cpp
+++ boj/10845.cpp
@@ -15,15 +15,18 @@ int main(void)
   for(int i = 0 ; i < n ; i++)
   {
     scanf("%s", s);
+    // pop 일 경우 맨 앞 원소 출력 후 삭제해줌
     if(!strcmp(s, "pop"))
     {
       printf("%d\n", v.size() == 0 ? -1 : v.front());
       if(v.size()) v.erase(v.begin());
     }
+    // size 일 경우 벡터의 사이즈 출력
     else if(!strcmp(s, "size"))
     {
       printf("%lu\n", v.size());
     }
+    // empty 는 벡터의 사이즈 기준으로 판단, empty() 도 있음
     else if(!strcmp(s, "empty"))
     {
       printf("%d\n", v.size() == 0 ? 1 : 0);
@@ -36,6 +39,7 @@ int main(void)
     {
       printf("%d\n", v.size() == 0 ? -1 : v.back());
     }
+    // push 일 경우 정수 하나를 더 입력받아서 벡터의 맨 뒤에 넣어준다
     else
     {
       scanf("%d",&pushData);
