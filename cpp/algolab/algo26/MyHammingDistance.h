3ad24225d48e179cb9ff88cd21e2885e12442256
bconfiden2 <bconfiden2@naver.com>
2020-05-12 17:59:58 +0900

    0512(3)

---
+++ cpp/algolab/algo26/MyHammingDistance.h
@@ -0,0 +1,20 @@
+#ifndef _MY_HAMMING_DISTANCE_H_
+#define _MY_HAMMING_DISTANCE_H_
+
+class MyBinaryNumber
+{
+public:
+// constructors
+  MyBinaryNumber ();
+  MyBinaryNumber (unsigned int val);
+// accessor/mutator functions
+  unsigned int getValue() const;
+  void setValue(unsigned int val);
+// utility function
+  int getHammingWeight(unsigned int target) const;
+  int getHammingDistance(const MyBinaryNumber& bn) const;
+
+private:
+  unsigned int value;
+};
+#endif // _MY_HAMMING_DISTANCE_H_
