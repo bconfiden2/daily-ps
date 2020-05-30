07facc70dd408763cd08421a2bb8e4fbec1de531
bconfiden2 <bconfiden2@naver.com>
2020-05-30 12:46:06 +0900

    BOJ 1로 만들기

--- boj/steady/1463.cpp
+++ boj/steady/1463.cpp
@@ -1,76 +1,35 @@
 #include <iostream>
-#include <cmath>
 
+// 두 수 중 작은 값을 리턴
 #define MINI(a,b) a < b ? a : b
 
 using namespace std;
 
 int n;
-//int minV = 1;
+// n까지 각각 값을 구해놓을 배열
 int arr[1000001] = {0, 0, 1, 1,};
 
 int main(void)
 {
   cin >> n;
 
+  // 4번부터 시작
   for(int i = 4 ; i <= n ; i++)
   {
-    arr[i] = arr[i-1] + 1;
-    /*
-    for(int j = i-1 ; j >= i-3 ; j--)
-    {
-      int k = i - j + arr[j];
-      if(k < plus1) plus1 = k;
-    }
-    */
-    if(i % 2 == 0)
+    // 이번의 최솟값은 1차적으로 보았을 때 직전꺼에서 1만 더해준 값이다
+    arr[i] = arr[i-1] + 1; // 1을 빼던지
+    // 그런데 만약 2로 나눠떨어질 경우에는 2로 나눈 인덱스의 값+1 과 비교해본다
+    if(i % 2 == 0) // 2로 나누던지
     {
       arr[i] = MINI(arr[i/2] + 1, arr[i]);
     }
-    if(i % 3 == 0)
+    // 만약 3으로 나눠떨어질 경우에도 확인해준다
+    if(i % 3 == 0) // 3으로 나누던지
     {
       arr[i] = MINI(arr[i/3] + 1, arr[i]);
     }
-
-    //if(arr[i] < minV) minV = arr[i];
+    // 그러면 최종적으로 이번인덱스의 최솟값이 구해진다
   }
-
-/*
-  for(int i = 1 ; i <= n ; i++)
-  {
-    cout << arr[i] << " ";
-  }
-  cout << endl;
-*/
-  //arr[idx*3] = arr[idx] + 1;
-  //idx *= 3;
-/*
-  while(twoN < n)
-  {
-    twoN *= 2;
-    max++;
-  }
-
-  for(int i = 0 ; i < max ; i++)
-  {
-    for(int j = 0 ; j < max ; j++)
-    {
-      if(i + j > max) break;
-      cur = pow(2, i) * pow(3, j);
-      if(cur > n) break;
-      k = n - cur + i + j;
-      if(k < ans && k > 0) ans = k;
-    }
-  }
-  if(n == 1) ans = 0;
-*/
-/*
-for(int i = 1 ; i < 20 ; i++)
-{
-  cout << arr[i] << " ";
-}
-cout << endl;
-*/
 
   cout << arr[n] << '\n';
 }
