5a5c227f5ac4fa19124b64e7d315f951532aaa7d
bconfiden2 <bconfiden2@naver.com>
2020-10-29 08:42:37 +0900

    Day 29

---
+++ boj/2020/10.October/1762.py
@@ -0,0 +1,14 @@
+import sys
+n, m = map(int, input().split())
+adjs = [set() for i in range(n+1)]
+cnt = 0
+
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    adjs[a].add(b) if a < b else adjs[b].add(a)
+
+for sets in adjs:
+    for i in sets:
+        cnt += len(sets.intersection(adjs[i]))
+
+print(cnt)
