f8588d9c41bf27c5c050035af7aa2591397a2941
bconfiden2 <bconfiden2@naver.com>
2022-02-06 18:32:58 +0900

    .

--- boj/2022/02.February/1655.py
+++ boj/2022/02.February/1655.py
@@ -2,7 +2,7 @@ input()
 import sys, heapq
 sm, bg = [], []
 for v in sys.stdin:
-    # 일단 최소힙에 넣은 뒤
+    # 일단 최대힙에 넣은 뒤
     heapq.heappush(sm, -int(v))
     # 두 힙의 차이가 1 이하가 될때까지 밸런싱
     while len(sm)-len(bg) > 1:
@@ -12,5 +12,5 @@ for v in sys.stdin:
         tmp = heapq.heappop(bg)
         heapq.heappush(bg, -heapq.heappop(sm))
         heapq.heappush(sm, -tmp)
-    # 항상 최소힙의 탑에 중간값 유지
+    # 항상 최대힙의 탑에 중간값 유지
     print(-sm[0])
